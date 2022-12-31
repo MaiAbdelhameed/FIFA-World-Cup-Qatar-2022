f29cfa8ed070d4079a6af45f60c62eaf70aa3dca
Mai Abdelhameed <maiabdelhameed16@gmail.com>
2023-01-01 00:20:10 +0200

    encrypted-password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,17 @@
 //const config = require("../config/auth.config");
 const Users=require('../models/users');
 const jwt = require("jsonwebtoken");
-//var bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
+const password = "Admin@123";
+
+bcrypt.genSalt(saltRounds)
+  .then(salt => {
+    console.log('Salt: ', salt);
+    return bcrypt.hash(password, salt)
+  }).then(hash => {
+    console.log('Hash: ', hash);
+  }).catch(err => console.error(err.message))
 
 exports.signup = async (req, res) => {
     try {
@@ -9,7 +19,9 @@ exports.signup = async (req, res) => {
         if (!(user.username && user.pass && user.firstName && user.lastName && user.birthDate && user.gender && user.email && user.role)) {
             res.status(400).send("Please fill all required inputs");
         }
-        
+
+        const hashedPwd = await bcrypt.hash(req.body.pass, saltRounds);
+        user.pass=hashedPwd;
         return res.json(user);
     }
     catch (err) {
@@ -29,7 +41,8 @@ exports.login =  async (req, res) => {
             throw new Error("User is not found");
           }
 
-          if (req.body.pass !== user.pass) {
+          const ismatch = await bcrypt.compare(req.body.pass, user.pass);
+          if (ismatch) {
             throw new Error("Password is incorrect");
           }
       
